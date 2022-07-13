574f404c61ac4b5f2e3f870305cd9139a0ac700e
Massimo De Marchi <massimo@inkofpixel.com>
2022-07-13 23:36:15 +0200

    fix: Remove underscores from branch handle

--- app/helpers/deployment-helpers.ts
+++ app/helpers/deployment-helpers.ts
@@ -26,5 +26,5 @@ function getRelativePath(path: string) {
 }
 
 export function getBranchHandle(branch: string) {
-  return branch.replace(/\//g, "-");
+  return branch.replace(/\//g, "-").replace(/_/g, "-");
 }
