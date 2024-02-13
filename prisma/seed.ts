8aebe0b0e6003515849bce1d380ef2fc71b17202
Massimo De Marchi <massimo@inkofpixel.com>
2024-02-13 17:10:38 +0100

    fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,22 @@
 import { faker } from "@faker-js/faker";
 import { prisma } from "~/lib/db.server";
 import { createBranch } from "~/models/branch.server";
+import { createRepository } from "~/models/repository.server";
 
 const BRANCH_COUNT = 10;
 
 async function seed() {
+  const repo = await createRepository({
+    name: "ifixit",
+    owner: "iFixit",
+    dockerComposeDirectory: "./apps/strapi",
+  });
   for (let i = 0; i < BRANCH_COUNT; i++) {
     await createBranch({
       branchName: faker.git.branch(),
       cloneUrl: "https://github.com/iFixit/react-commerce.git",
       dockerComposeDirectory: "./backend",
+      repositoryId: repo.id,
     });
   }
 
