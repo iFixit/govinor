8bda493fce2d34a1098dc4c7d154a04ce9bfeb26
Massimo De Marchi <massimo@inkofpixel.com>
2024-02-13 18:01:06 +0100

    keep cjs format for the time being

--- remix.config.js
+++ remix.config.js
@@ -4,10 +4,12 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverModuleFormat: "cjs",
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
     v2_meta: true,
+    v2_headers: true,
   },
 };
