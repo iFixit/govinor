efd21e5d50e757be139f33bca560bc154df56643
Massimo De Marchi <massimo@inkofpixel.com>
2024-03-19 15:20:23 +0100

    fix waiting status

--- app/components/status-indicator.tsx
+++ app/components/status-indicator.tsx
@@ -14,7 +14,7 @@ export function StatusIndicator({ className, status }: StatusIndicatorProps) {
         status === "failed" && "bg-rose-400/10 text-rose-400",
         status === "delayed" && "bg-yellow-400/10 text-yellow-400",
         status === "active" && "bg-blue-400/10 text-blue-400",
-        status === "wait" && "bg-orange-400/10 text-orange-400",
+        status === "waiting" && "bg-orange-400/10 text-orange-400",
         status === "paused" && "bg-purple-400/10 text-purple-400",
         status === "repeat" && "bg-gray-100/10 text-gray-500",
         className
